5fcb5120b1d01e2761c21f681a9bda83b2df4b0b
Abdirahim Hussein <drahimhussein@gmail.com>
2023-01-16 18:30:27 +0000

    create a react app and completed a file with my name

---
+++ my-app/Abdirahim.js
@@ -0,0 +1,10 @@
+function Abdirahim() {
+    const appName = "Abdirahim Hussein";
+    return (
+      <div>
+        <h1>My name is {appName}</h1>
+      </div>
+    );
+  }
+  ​
+  export default Abdirahim;
