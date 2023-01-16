64cf1eca8557a4ccc8c3cd513e2be833751d783d
Abdirahim Hussein <drahimhussein@gmail.com>
2023-01-16 23:24:16 +0000

    completed pokedex app with react solved npm problems

---
+++ pokedex/src/Abdirahim.js
@@ -0,0 +1,9 @@
+function Abdirahim() {
+    const appName = "Abdirahim Hussein";
+    return (
+      <div>
+        <h1>My name is {appName}</h1>
+      </div>
+    );
+  }
+  export default Abdirahim;
---
+++ pokedex/src/App.js
@@ -0,0 +1,28 @@
+import Logo from "./Logo";
+import Abdirahim from "./Abdirahim";
+// import BestPokemon from "./BestPokemon";
+import CaughtPokemon from "./CaughtPokemon";
+import PokemonMovesSelector from "./PokemonMovesSelector";
+import PokemonCity from "./PokemonCity";
+
+function App() {
+  const abilities = ["Anticipation", "Adaptability", "Run-Away"];
+    function logWhenClicked() {
+      console.log("when logo is clicked");
+    }
+  const appName = "Abdirahim";
+  const date = new Date().toLocaleDateString();
+  return (
+    <div>
+      <header>
+        <h1>My name is {appName} </h1>
+        <Logo appName="Pokedex" handleClick={logWhenClicked}/>
+        <Abdirahim/>
+        <CaughtPokemon pokemonDate={date} />
+        <PokemonMovesSelector />
+        <PokemonCity />
+      </header>
+    </div>
+  );
+}
+export default App;
